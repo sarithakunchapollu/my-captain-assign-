f14904052d218b25904624a5fac1fe6d856c1b5a
sarithakunchapollu <111043979+sarithakunchapollu@users.noreply.github.com>
2022-08-16 23:59:01 +0530

    Create Spy 1.py

---
+++ Spy 1.py
@@ -0,0 +1,10 @@
+#import file extension
+
+filename =input("input the file name is:")
+f_exts=filename.split(".")
+print("the extension of the file is."+repr(f_exts{-1}))
+
+
+
+Output:-
+Extension of the file is:'java'
