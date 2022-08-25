693fcc0976a82e652f5806b1dc0e02eec7931bb1
sarithakunchapollu <111043979+sarithakunchapollu@users.noreply.github.com>
2022-08-25 21:14:07 +0530

    Create Spy 3.py

---
+++ Spy 3.py
@@ -0,0 +1,13 @@
+# Python program to print positive Numbers in given range
+ 
+start, end = -4, 19
+ 
+#each number in list
+for num in range(start, end + 1):
+     
+    # checking condition
+    if num >= 0:
+        print(num, end = " ")
+
+Output
+>0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19
